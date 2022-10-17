ce1095b3aba08453274e7646130066a07708b1ff
Noa Vukasovic <noavukasovic@gmail.com>
2022-10-17 13:27:00 +0200

    Put visual Menu in main_menu

--- src/main.c
+++ src/main.c
@@ -3,204 +3,172 @@
 #define HIGHEST_OPTION 13
 #define LOWEST_OPTION 0
 
+void
+main_menu()
+{
+	struct Node* head = NULL;
+	struct Node* tail = NULL;
+	struct Node* tmp  = NULL;
+
+	int option = -1;
+	int number;
+	char sorted = 0;
+	while (option != 0)
+	{
+		printf("==============================================\n");
+		printf("============== SELECT AN OPTION ==============\n");
+		printf("==============================================\n");
+
+		printf("\t    1. Push in List\n");
+		printf("\t    2. Put in List\n");
+		printf("\t    3. Insert in a sorted list\n");
+		printf("\t    4. Search for Element\n");
+		printf("\t    5. Remove from List\n");
+		printf("\t    6. Reverse List\n");
+		printf("\t    7. Print List\n");
+		printf("\t    8. Generate random List\n");
+		printf("\t    9. Selection Sort a list\n");
+		printf("\t   10. Bubble Sort a list\n");
+		printf("\t   11. Merge Sort a list\n");
+		printf("\t   12. Quick Sort a list\n");
+		printf("\t   13. Destroy a list\n");
+		printf("\t    0. Exit\n");
+
+		printf("\n: ");
+		scanf("%d", &option);
+
+		if (option < LOWEST_OPTION || option > HIGHEST_OPTION)
+		{
+			printf("\n\tERROR: That option doesn't exist! Try again.\n\n");
+			continue;
+		}
+
+		switch (option)
+		{
+			case 1:
+				printf("Which element would you like to PUSH?\nElement: ");
+				scanf("%d<CR>", &number);
+
+				push(&head, number, &tail);
+
+				if (head->next != NULL)
+				{
+					if (head->data > head->next->data)
+						sorted = 0;
+				}
+				break;
+
+			case 2:
+				printf("Which element would you like to PUT?\nElement: ");
+				scanf("%d<CR>", &number);
+
+				put(&head, number, &tail);
+				tmp = head;
+				while (tmp->next)
+				{
+					if (tmp->next == tail)
+					{
+						if (tmp->data > tail->data)
+							sorted = 0;
+
+						break;
+					}
+					else
+						tmp = tmp->next;
+				}
+
+				break;
+
+			case 3:
+				if (sorted)
+				{
+					printf("Which element would you like to INSERT in a sorted list?\nElement: ");
+					scanf("%d<CR>", &number);
+
+					insert(&head, number);
+				}
+				else
+					printf("\n\tERROR: A list isn't sorted. Try to sort it first.\n\n");
+
+				break;
+
+			case 4:
+				printf("Search for Element: ");
+				scanf("%d", &number);
+				int found = find(head, number);
+
+				if (found)
+					printf("\n\tElement %d exists in the List!\n\n", number);
+				else
+					printf("\n\tElement %d does NOT exit in the List!\n\n", number);
+
+				break;
+
+			case 5:
+				printf("Which element would you like to DELETE?\nElement: ");
+				scanf("%d<CR>", &number);
+
+				erase(&head, number, &tail);
+				break;
+
+			case 6:
+				reverse_list(&head);	
+				break;
+
+			case 7:
+				printf("\tCurrent List:");
+				print_list(head);
+				break;
+
+			case 8:
+				generate_list(&head, &tail);
+				break;
+
+			case 9:
+				selection_sort(head);
+				sorted = 1;
+				break;
+
+			case 10:
+				bubble_sort(head);
+				sorted = 1;
+				break;
+
+			case 11:
+				merge_sort(&head);
+				sorted = 1;
+				if (head == NULL || head->next == NULL)
+					printf("\n\tThere's nothing to sort!\n\n");
+				else
+					printf("\n\tA List has been successfully Merge-sorted!\n\n");
+
+				break;
+
+			case 12:
+				quick_sort(head, tail);
+				sorted = 1;
+				if (head == NULL || head->next == NULL)
+					printf("\n\tThere's nothing to sort!\n\n");
+				else
+					printf("\n\tA List has been successfully Quick-sorted!\n\n");
+
+				break;
+
+			case 13: 
+				destroy_list(&head);
+				break;
+
+			case 0:
+				option = 0;
+				continue;
+		}
+	}
+}
+
 int
 main()
 {
-	//struct Node* head = NULL;
-	//struct Node* tail = NULL;
-	//struct Node* tmp  = NULL;
-
-	//int option = -1;
-	//int number;
-	//char sorted = 0;
-	//while (option != 0)
-	//{
-	//	printf("==============================================\n");
-	//	printf("============== SELECT AN OPTION ==============\n");
-	//	printf("==============================================\n");
-
-	//	printf("\t    1. Push in List\n");
-	//	printf("\t    2. Put in List\n");
-	//	printf("\t    3. Insert in a sorted list\n");
-	//	printf("\t    4. Search for Element\n");
-	//	printf("\t    5. Remove from List\n");
-	//	printf("\t    6. Reverse List\n");
-	//	printf("\t    7. Print List\n");
-	//	printf("\t    8. Generate random List\n");
-	//	printf("\t    9. Selection Sort a list\n");
-	//	printf("\t   10. Bubble Sort a list\n");
-	//	printf("\t   11. Merge Sort a list\n");
-	//	printf("\t   12. Quick Sort a list\n");
-	//	printf("\t   13. Destroy a list\n");
-	//	printf("\t    0. Exit\n");
-
-	//	printf("\n: ");
-	//	scanf("%d", &option);
-
-	//	if (option < LOWEST_OPTION || option > HIGHEST_OPTION)
-	//	{
-	//		printf("\n\tERROR: That option doesn't exist! Try again.\n\n");
-	//		continue;
-	//	}
-
-	//	switch (option)
-	//	{
-	//		case 1:
-	//			printf("Which element would you like to PUSH?\nElement: ");
-	//			scanf("%d<CR>", &number);
-
-	//			push(&head, number, &tail);
-
-	//			if (head->next != NULL)
-	//			{
-	//				if (head->data > head->next->data)
-	//					sorted = 0;
-	//			}
-	//			break;
-
-	//		case 2:
-	//			printf("Which element would you like to PUT?\nElement: ");
-	//			scanf("%d<CR>", &number);
-
-	//			put(&head, number, &tail);
-	//			tmp = head;
-	//			while (tmp->next)
-	//			{
-	//				if (tmp->next == tail)
-	//				{
-	//					if (tmp->data > tail->data)
-	//						sorted = 0;
-
-	//					break;
-	//				}
-	//				else
-	//					tmp = tmp->next;
-	//			}
-
-	//			break;
-
-	//		case 3:
-	//			if (sorted)
-	//			{
-	//				printf("Which element would you like to INSERT in a sorted list?\nElement: ");
-	//				scanf("%d<CR>", &number);
-
-	//				insert(&head, number);
-	//			}
-	//			else
-	//				printf("\n\tERROR: A list isn't sorted. Try to sort it first.\n\n");
-
-	//			break;
-
-	//		case 4:
-	//			printf("Search for Element: ");
-	//			scanf("%d", &number);
-	//			int found = find(head, number);
-
-	//			if (found)
-	//				printf("\n\tElement %d exists in the List!\n\n", number);
-	//			else
-	//				printf("\n\tElement %d does NOT exit in the List!\n\n", number);
-
-	//			break;
-
-	//		case 5:
-	//			printf("Which element would you like to DELETE?\nElement: ");
-	//			scanf("%d<CR>", &number);
-
-	//			erase(&head, number, &tail);
-	//			break;
-
-	//		case 6:
-	//			reverse_list(&head);	
-	//			break;
-
-	//		case 7:
-	//			printf("\tCurrent List:");
-	//			print_list(head);
-	//			break;
-
-	//		case 8:
-	//			generate_list(&head, &tail);
-	//			break;
-
-	//		case 9:
-	//			selection_sort(head);
-	//			sorted = 1;
-	//			break;
-
-	//		case 10:
-	//			bubble_sort(head);
-	//			sorted = 1;
-	//			break;
-
-	//		case 11:
-	//			merge_sort(&head);
-	//			sorted = 1;
-	//			if (head == NULL || head->next == NULL)
-	//				printf("\n\tThere's nothing to sort!\n\n");
-	//			else
-	//				printf("\n\tA List has been successfully Merge-sorted!\n\n");
-
-	//			break;
-
-	//		case 12:
-	//			quick_sort(head, tail);
-	//			sorted = 1;
-	//			if (head == NULL || head->next == NULL)
-	//				printf("\n\tThere's nothing to sort!\n\n");
-	//			else
-	//				printf("\n\tA List has been successfully Quick-sorted!\n\n");
-
-	//			break;
-
-	//		case 13: 
-	//			destroy_list(&head);
-	//			break;
-
-	//		case 0:
-	//			option = 0;
-	//			continue;
-	//	}
-	//}
-
-	struct Node* head = NULL;
-	struct Node* tail = NULL; // Comment for TEST 1 to prevent Warnings
-
-	// -----------------------
-	// ------- TESTING -------
-	// -----------------------
-
-	// TEST 1
-	// Empty List
-
-	// TEST 2
-	// put(&head, 11, &tail);
-	// put(&head, 51, &tail);
-
-	// TEST 3
-	// put(&head, 11, &tail);
-	// put(&head, 12, &tail);
-	// put(&head, 51, &tail);
-	// put(&head, 52, &tail);
-
-	// TEST 4
-	put(&head, 11, &tail);
-	put(&head, 12, &tail);
-	put(&head, 13, &tail);
-	put(&head, 14, &tail);
-	put(&head, 51, &tail);
-	put(&head, 52, &tail);
-	put(&head, 53, &tail);
-	put(&head, 54, &tail);
-
-	printf("Before:");
-	print_list(head);
-
-	the_runner_technique(head);
-
-	printf("After:");
-	print_list(head);
+	main_menu();
 
     return 0;
 }
